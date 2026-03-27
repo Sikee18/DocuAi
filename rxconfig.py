8e609539b66dc73e3dd31927d2a2aea7390028f7
Sikee18 <rathu1829@gmail.com>
2026-03-27 14:14:38 +0530

    Allow HF Space domain in CORS Websocket config

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="DOCU_AI",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://sikee18-docuai.hf.space"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
